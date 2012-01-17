1bdb4b4771d5e84e1ea252366b180d760b09f1f5
Mario <mario.antunes@ua.pt>
2012-01-17 13:06:21 +0000

    Added a simulation for performance evaluation
    Added:
    1. Monte carlo simulation based on Luis Torgo KDD classes
    2. Added a embedded dataset generate from a time series
    3. Added MSE, MAD and MAPE error estimators
    4. Added a SVM machine to generate non-linear models

--- code/code.R
+++ code/code.R
@@ -1,6 +1,7 @@
 library(zoo)
 library(tseries)
-library(forecast)
+library(TTR)
+library(e1071)
 
 ###############################################################################
 # User defined functions
@@ -16,6 +17,15 @@ pre.processing <- function(ts.data)
 }
 
 # Evaluation metrics
+
+# Mean square error (MSE)
+
+mse <- function(preds, trues)
+{
+    error <- mean((preds - trues)^2, na.rm = T)
+
+    return(error)
+}
 
 # Mean absolute percentage error (MAPE)
 mape <- function(preds, trues)
@@ -45,13 +55,80 @@ embedded.dataset <- function(data, dimension = 3, tlag = 1)
         names <- cbind(names,paste("pr",i,sep = ""))
     }
     colnames(dataset) <- names
-    rows <- (dimension*tlag):(nrow(data))
-    dataset <- dataset[rows,]
+    #rows <- (dimension*tlag):(nrow(data))
+    #dataset <- dataset[rows,]
+
+    dataset <- na.omit(dataset)
 
     return(dataset)
 }
 
 
+# Monte Carlo simulation with sliding window
+#
+# Provided in KDD MapI class
+MonteCarlo.estimates <- function(data, nreps=10, train.size, test.size, learner, learner.pars, perf.func, relearn.step=1, starting.points=NULL)
+{
+  data <- as.data.frame(data)
+  n <- nrow(data)
+  
+  if (is.null(starting.points) || missing(starting.points))
+  {
+    selection.range <- (train.size+1):(n-test.size+1)
+    starting.points <- sort(sample(selection.range,nreps))
+  }
+  preds <- list()
+  
+  for(it in seq(along=starting.points))
+  {
+    start <- starting.points[it]
+    cat('Going for sliding window testing from row ',start,'(train.sz=',train.size,',Relearn=',relearn.step,')\n')
+    preds[[it]] <- sliding.window.testing(data[1:(start+test.size-1),], train.size, learner, learner.pars,relearn.step, test.pos=start)
+      
+    if(exists('MC.results'))
+      MC.results[it,] <- do.call(perf.func,list(preds[[it]],data[start:(start+test.size-1),],learner.pars))
+    else
+      MC.results <- data.frame(do.call(perf.func,list(preds[[it]],data[start:(start+test.size-1),],learner.pars)))
+  }
+
+  list(Starting.Points=starting.points,Results=MC.results,Predictions=preds)
+}
+
+sliding.window.testing <- function(orig.data, window.size, learner, learner.pars, relearn.step=1, test.pos=window.size+1)
+{
+    init.test <- test.pos
+    n <- nrow(orig.data)
+    preds <- vector()
+    while (test.pos <= n)
+    {
+        cat('*')
+        learner.pars$data <- orig.data[(test.pos-window.size):(test.pos-1),]
+        model <- do.call(learner,learner.pars)
+        preds <- c(preds,predict(model,orig.data[test.pos:min(n,test.pos+relearn.step-1),]))
+        test.pos <- test.pos+relearn.step
+    }
+    cat('\n')
+
+    return(preds)
+}
+
+target.name <- function(form, data)
+{
+    mt <- terms(form,data=data)
+    if ((yvar <- attr(mt, "response")) <= 0)
+    {
+        stop(paste("Incorrect response variable",yvar))
+    }
+    as.character(attr(mt, "variables"))[-1][yvar]
+}
+
+error.func <- function(preds, test.data, learner.pars)
+{
+    form <- learner.pars[[1]]  # assumes the first argument is the formula
+    list(mse = mse(preds,test.data[,target.name(form,test.data)]),
+    mape = mape(preds,test.data[,target.name(form,test.data)]),
+    mad = mad(preds,test.data[,target.name(form,test.data)]))
+}
 
 ###############################################################################
 # Code
@@ -93,10 +170,13 @@ cat("MAD :",mad(preds.window.12, ts.data.processed),"\n")
 # Exponencial moving average 
 cat("Exponencial moving average\n")
 preds <- EMA(ts.data.processed)
+cat("MSE :",mse(preds, ts.data.processed),"\n")
 cat("MAPE:",mape(preds, ts.data.processed),"\n")
 cat("MAD :",mad(preds, ts.data.processed),"\n")
 
 # Generate simple dataset
-simple.dataset <- embedded.dataset(ts.data)
+dataset <- embedded.dataset(ts.data,13,1)
 
-
+# Monte carlo simulation
+res <- MonteCarlo.estimates(coredata(dataset),nreps=10,train.size=100,test.size=10,'svm',
+list(fr ~ .,type='eps-regression', kernel='sigmoid'),'error.func',relearn.step=50)
